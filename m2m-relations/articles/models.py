d8fc798603fb6b16598ed6ee91cd502945fa10b2
volivanmail <volivanmail@gmail.com>
2021-12-09 02:06:47 +0300

    added a second task

---
+++ m2m-relations/articles/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    articles = models.ManyToManyField(Article, related_name='tag', through='ArticleScopes')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.title
+
+
+class ArticleScopes(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    scope = models.ForeignKey(Scope, on_delete=models.CASCADE)
+    is_main = models.BooleanField()
+
+
